c32e3d7ab1bd14223ea96c7396dc946b6bfe5366
mosmn <ithinkisee8@gmail.com>
2023-06-23 16:18:07 +0800

    added append

--- linkedList.js
+++ linkedList.js
@@ -2,11 +2,35 @@
 
 class Node {
     constructor(value = null, nextNode = null) {
-        this.value = value;
-        this.nextNode = nextNode;
+      this.value = value;
+      this.nextNode = nextNode;
     }
 }
-
+  
 class LinkedList {
-
+    constructor() {
+      this.head = null;
+      this.tail = null;
+    }
+  
+    append(value) {
+      const newNode = new Node(value);
+  
+      if (!this.head) {
+        this.head = newNode;
+        this.tail = newNode;
+      } else {
+        this.tail.nextNode = newNode;
+        this.tail = newNode;
+      }
+    }
 }
+  
+const linkedList = new LinkedList();
+
+linkedList.append(10);
+linkedList.append(20);
+linkedList.append(30);
+
+console.log(linkedList);
+
