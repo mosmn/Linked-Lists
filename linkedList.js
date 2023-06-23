50f3a46932ad005daceea96b3bc40f1d9e4dc3ea
mosmn <ithinkisee8@gmail.com>
2023-06-23 16:05:07 +0800

    initial setup

---
+++ linkedList.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+class Node {
+    constructor(value = null, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
+
+class LinkedList {
+
+}
